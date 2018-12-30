c8559fd66ce489e37a6123faa76ec63e50521ec0
Kyle Houghton <kylebenz@hotmail.com>
2018-12-30 03:23:03 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -19,7 +19,7 @@ end
 def word_substituter(tweet)
   tweet_a = tweet.to_a
   tweet_a.each_with_index do |word, index|
-    
+    tweet_a[index] = dictionary[word] if 
   end
   #dictionary[wrd]
 end
