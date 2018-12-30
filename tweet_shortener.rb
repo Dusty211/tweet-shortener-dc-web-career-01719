1a87cdb793d9a1ea47f8c0541192449cb6c093c7
Kyle Houghton <kylebenz@hotmail.com>
2018-12-30 04:55:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,6 +38,7 @@ def selective_tweet_shortener(tweet)
   tweet.split.join('').split('').length < 130 ? tweet : bulk_tweet_shortener(tweet)
 end
 
+selective_tweet_shortener(tweet)
 
 def shortened_tweet_truncator(tweet)
 end
